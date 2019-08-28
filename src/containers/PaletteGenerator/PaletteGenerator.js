40a04fefd25850ef891837029af1979820499bb5
“Hindreen” <hndfaw@gmail.com>
2019-08-27 20:51:41 -0600

    Add lock to generate colors

--- src/containers/PaletteGenerator/PaletteGenerator.js
+++ src/containers/PaletteGenerator/PaletteGenerator.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setGeneratedColors, openPaletteGenerator } from '../../actions';
 import './PaletteGenerator.scss';
-import { parseExpression } from '@babel/parser';
 var ColorScheme = require('color-scheme');
 
 export class PaletteGenerator extends Component {
@@ -15,7 +14,7 @@ export class PaletteGenerator extends Component {
             color_4: '',
             color_5: '',
             paletteName: '',
-            frozen: [false, false, false, false, false]
+            frozen: ['Lock', 'Lock', 'Lock', 'Lock', 'Lock']
         }
     }
 
@@ -34,11 +33,11 @@ export class PaletteGenerator extends Component {
         var palette = colors.map(color => '#' + color).slice(10, 15);
 
         this.setState({
-            color_1: this.state.frozen[0] ? this.state.color_1 : palette[0],
-            color_2: this.state.frozen[1] ? this.state.color_2 : palette[1],
-            color_3: this.state.frozen[2] ? this.state.color_3 : palette[2],
-            color_4: this.state.frozen[3] ? this.state.color_4 : palette[3],
-            color_5: this.state.frozen[4] ? this.state.color_5 : palette[4]
+            color_1: this.state.frozen[0] === 'Locked' ? this.state.color_1 : palette[0],
+            color_2: this.state.frozen[1] === 'Locked' ? this.state.color_2 : palette[1],
+            color_3: this.state.frozen[2] === 'Locked' ? this.state.color_3 : palette[2],
+            color_4: this.state.frozen[3] === 'Locked' ? this.state.color_4 : palette[3],
+            color_5: this.state.frozen[4] === 'Locked' ? this.state.color_5 : palette[4]
         });
         this.props.handleSetGeneratedColors(palette);
         
@@ -48,7 +47,11 @@ export class PaletteGenerator extends Component {
     handleFrozen = e => {
         const index = parseInt(e.target.id);
         const value = this.state.frozen 
-        value[index] = !value[index]
+        if (value[index] === 'Lock') {
+            value[index] = 'Locked'
+        }  else {
+            value[index] = 'Lock'
+        }
         this.setState({frozen:  value})
     }
 
@@ -58,7 +61,6 @@ export class PaletteGenerator extends Component {
 
     handleOnChange = e => {
         this.setState({paletteName: e.target.value});
-        
     }
 
     submitNewPalette = () => {
@@ -69,7 +71,7 @@ export class PaletteGenerator extends Component {
             this.props.handleOpenPaletteGenerator()
             this.clearInput()
             this.setState({
-                frozen: [false, false, false, false, false]
+                frozen: ['Lock', 'Lock', 'Lock', 'Lock', 'Lock']
             });
         }
     }
@@ -80,17 +82,19 @@ export class PaletteGenerator extends Component {
 
 
     render() {
+        const { frozen } = this.state
+
         const { open } = this.props.openPaletteGen;
         const palleteGenstyle = open ? {display: 'flex'} : {display: 'none'}
-    
+        
         return (
             <div className="palette-gen-container" style={palleteGenstyle}>
                 <article  className="container" >
-                    <section className="color color-1" onClick={this.handleFrozen} id="0" style={{background: this.state.color_1}}></section>
-                    <section className="color color-2" onClick={this.handleFrozen} id="1" style={{background: this.state.color_2}}/>
-                    <section className="color color-3" onClick={this.handleFrozen} id="2" style={{background: this.state.color_3}}/>
-                    <section className="color color-4" onClick={this.handleFrozen} id="3" style={{background: this.state.color_4}}/>
-                    <section className="color color-5" onClick={this.handleFrozen} id="4" style={{background: this.state.color_5}}/>
+                    <section className="color color-1" onClick={this.handleFrozen} id="0" style={{background: this.state.color_1}}>{frozen[0]}</section>
+                    <section className="color color-2" onClick={this.handleFrozen} id="1" style={{background: this.state.color_2}}>{frozen[1]}</section>
+                    <section className="color color-3" onClick={this.handleFrozen} id="2" style={{background: this.state.color_3}}>{frozen[2]}</section>
+                    <section className="color color-4" onClick={this.handleFrozen} id="3" style={{background: this.state.color_4}}>{frozen[3]}</section>
+                    <section className="color color-5" onClick={this.handleFrozen} id="4" style={{background: this.state.color_5}}>{frozen[4]}</section>
                     <input type="text" maxLength="35" value={this.state.paletteName} className="palette-name-input" placeholder="Plette name" onChange={this.handleOnChange}/>
                     <button className="gen-add-btn generate-btn" onClick={this.generatePalette} >Generate</button>
                     <button className="gen-add-btn add-update-palette-btn" onClick={this.submitNewPalette}
